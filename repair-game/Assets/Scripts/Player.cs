3e63a84e48ba6f620219458af045be87a7a4798c
Anton Pantev <antonpantev123@gmail.com>
2020-02-01 13:27:08 -0500

    Adding player script that raycasts on click.

---
+++ repair-game/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100.0f))
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+}
