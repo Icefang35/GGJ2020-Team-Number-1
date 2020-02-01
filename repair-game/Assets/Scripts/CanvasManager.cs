2ea69cf045a2800951e3611459540811ed506bd4
Austen Gralheer <gralheerab@mymail.vcu.edu>
2020-02-01 15:50:17 -0500

    added pause menu and canvas manager script

---
+++ repair-game/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CanvasManager : MonoBehaviour {
+    public Button continueButton;
+    public Button quitButton;
+    public Canvas pauseBox;
+    public GameObject player;
+    public void Start()
+    {
+        pauseBox.GetComponent<Canvas>().enabled = false;
+        quitButton.onClick.AddListener(quitGame);
+        continueButton.onClick.AddListener(closeMenu);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            openMenu();
+        }
+    }
+
+    void openMenu() {
+        pauseBox.GetComponent<Canvas>().enabled = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        player.GetComponent <Toolbox.FirstPersonCamera>().enabled = false;
+    }
+
+    void closeMenu() {
+        pauseBox.GetComponent<Canvas>().enabled = false;
+        player.GetComponent<Toolbox.FirstPersonCamera>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void quitGame() {
+        Application.Quit();
+    }
+}
