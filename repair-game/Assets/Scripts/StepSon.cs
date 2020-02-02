2008111372ef4840bf3986fa2ddb4fc2f750f040
Anton Pantev <antonpantev123@gmail.com>
2020-02-01 19:26:07 -0500

    Moving the step son to a target location.

---
+++ repair-game/Assets/Scripts/StepSon.cs
@@ -0,0 +1,21 @@
+﻿using Toolbox;
+using UnityEngine;
+
+public class StepSon : MonoBehaviour
+{
+    public Transform target;
+
+    Movement3D movement;
+    Arrive3D arrive;
+
+    void Start()
+    {
+        movement = GetComponent<Movement3D>();
+        arrive = GetComponent<Arrive3D>();
+    }
+
+    void FixedUpdate()
+    {
+        movement.steering = arrive.GetSteering(target.position);
+    }
+}
