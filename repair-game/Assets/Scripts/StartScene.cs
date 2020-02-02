4b8bc75b8bf6037cbd45aedfdfe4b0b9897754dc
Austen Gralheer <gralheerab@mymail.vcu.edu>
2020-02-02 11:40:52 -0500

    added new step child model and got the startscene to work

--- repair-game/Assets/Scripts/StartScene.cs
+++ repair-game/Assets/Scripts/StartScene.cs
@@ -1,25 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartScene : MonoBehaviour
 { 
-    // public Button continue;
-    public Button Back;
-    public Button Quit;
+    public Button btnContinue;
+    public Button btnBack;
+    public Button btnQuit;
+    public Button btnPlay;
 
+    public Canvas NextCanvas;
     public Canvas CurrentCanvas;
+    public Canvas PreviousCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (btnContinue != null)
+        {
+            btnContinue.onClick.AddListener(Continue);
+        }
+        else
+        {
+            btnPlay.onClick.AddListener(Play);
+        }
+        if (btnBack != null)
+        {
+            btnBack.onClick.AddListener(Back);
+        }
+        else
+        {
+            btnQuit.onClick.AddListener(Quit);
+        }
+    }
+
+    void Continue()
+    {
+        NextCanvas.enabled = true;
+        CurrentCanvas.enabled = false;
+    }
+
+    void Back()
+    {
+        PreviousCanvas.enabled = true;
+        CurrentCanvas.enabled = false;
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void Quit()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
