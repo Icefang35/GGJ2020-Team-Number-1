43c25306fc442b7dd469f21e770b941176016461
Austen Gralheer <gralheerab@mymail.vcu.edu>
2020-02-01 21:08:15 -0500

    addedstopping to stepchild

--- repair-game/Assets/Scripts/StepSonSensor.cs
+++ repair-game/Assets/Scripts/StepSonSensor.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Toolbox;
 
 public class StepSonSensor : MonoBehaviour
 {
     ParentItem parentItem;
+    public float waitTime = 3f;
 
     void Start()
     {
         parentItem = GetComponentInParent<ParentItem>();
     }
 
-    void OnTriggerEnter(Collider other)
+    IEnumerator OnTriggerEnter(Collider other)
     {
         if (other.name == "StepSon" && parentItem.IsComplete())
         {
+            other.GetComponent<StepSon>().enabled = false;
+            other.GetComponent<Movement3D>().steering = Steering3D.Stop;
+            yield return new WaitForSeconds(waitTime);
             parentItem.Explode();
+            other.GetComponent<StepSon>().enabled = true;
+
         }
     }
 }
