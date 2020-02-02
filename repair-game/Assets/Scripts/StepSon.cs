f9ef4918f6c6d3908bf68fde2401c838d22b3494
Anton Pantev <antonpantev123@gmail.com>
2020-02-01 19:35:43 -0500

    Made the step son loop through a path.

--- repair-game/Assets/Scripts/StepSon.cs
+++ repair-game/Assets/Scripts/StepSon.cs
@@ -3,19 +3,32 @@ using UnityEngine;
 
 public class StepSon : MonoBehaviour
 {
-    public Transform target;
+    public Transform path;
 
     Movement3D movement;
     Arrive3D arrive;
+
+    int targetIndex = 0;
+    Transform target;
 
     void Start()
     {
         movement = GetComponent<Movement3D>();
         arrive = GetComponent<Arrive3D>();
+
+        targetIndex = 0;
+        target = path.GetChild(0);
     }
 
     void FixedUpdate()
     {
         movement.steering = arrive.GetSteering(target.position);
+
+        if (!movement.steering.isMoving)
+        {
+            targetIndex++;
+            targetIndex %= path.childCount;
+            target = path.GetChild(targetIndex);
+        }
     }
 }
